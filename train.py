f538ef12b28a112d8dda064712d6e66be4a48c08
Yitong <yitongma7@gmail.com>
2019-08-26 19:16:58 -0700

    from GPUowitz

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-#%%
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,48 +11,57 @@ from os import listdir
 from genTrainSet import artSamples
 from PIL import Image
 
-#%%
 groundtruth = pd.read_excel('./trainingSet/groundTruth.xlsx', header=None)
 groundtruth = np.array(groundtruth).flatten()
 
 oriSamples = []
 for idx in range(len(groundtruth)):
-    imgArr = np.array(Image.open('{0}/trainSample_{1:03d}.tiff'.format('./trainingSet', idx)))
+    img = Image.open('{0}/trainSample_{1:03d}.tiff'.format('./trainingSet', idx))
+    img = img.resize((640, 480))
+    imgArr = np.array(img)
     oriSamples.append(imgArr)
 
+plt.imshow(oriSamples[0])
+
 allSamples =np.array([img[:, :, np.newaxis] for img in artSamples(oriSamples)])
-allTruths = np.repeat(groundtruth, 4)
+allTruths = keras.utils.to_categorical(np.repeat(groundtruth, 4))
 
+normedSamples = (allSamples - allSamples.mean(0)) / allSamples.max()
 
-
-#%%
 model = Sequential()
-model.add(Conv2D(16, (3, 3), padding='same', input_shape=(960, 1280, 1)))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(8, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
-
+model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=(480, 640, 1)))
+model.add(Conv2D(16, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(3, 3)))
 model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dense(32))
-model.add(Activation('relu'))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(2, activation='softmax'))
 
+# model.add(Conv2D(16, (3, 3), padding='same', input_shape=(960, 1280, 1)))
+# # model.add(BatchNormalization())
+# model.add(Activation('relu'))
+
+# model.add(Conv2D(8, (3, 3), padding='same'))
+# # model.add(BatchNormalization())
+# model.add(Activation('relu'))
+
+# model.add(MaxPooling2D(pool_size=(3, 3)))
+# model.add(Dropout(0.5))
+
+# model.add(Flatten())
+# model.add(Dense(128))
+# model.add(Activation('relu'))
+# model.add(Dense(32))
+# model.add(Activation('relu'))
+# model.add(Dense(2))
+# model.add(Activation('softmax'))
+
+# model.summary()
+
+model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
 model.summary()
-
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 print("Parameter number at {0}".format(model.count_params()))
-#%%
-history = model.fit(allSamples, allTruths, epochs=10, batch_size=16)
 
-
-#%%
+history = model.fit(allSamples, allTruths, epochs=20, batch_size=8)
