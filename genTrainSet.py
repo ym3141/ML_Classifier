e41fed358ac9dd8b1552a2967cb52610d03ecbd9
ym3141 <yma2@caltech.edu>
2019-08-26 19:30:35 -0700

    updated sample

--- genTrainSet.py
+++ genTrainSet.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 from PIL import Image
 from os import listdir
@@ -6,14 +7,14 @@ from shutil import copyfile
 imageDir1 = 'D:/Snapshots/2019-08-23_Scanning/Scan2019-08-23 22.23.41'
 imageDir2 = 'D:/Snapshots/2019-08-23_Scanning/Scan2019-08-25 22.27.41'
 
-def copySamples(targetDir):
+def copySamples(targetDir, shift):
     sampleList = ['{0}/{1}'.format(imageDir1, file) for file in listdir(imageDir1) if file.endswith('.tif')] + \
         ['{0}/{1}'.format(imageDir2, file) for file in listdir(imageDir2) if file.endswith('.tif')]
 
     randList = np.random.choice(sampleList, 40, False)
 
     for idx, file in enumerate(randList):
-        copyfile(file, '{0}/trainSample_{1:03d}.tiff'.format(targetDir, idx))
+        copyfile(file, '{0}/trainSample_{1:03d}.tiff'.format(targetDir, idx + shift))
     
 def artSamples(oriSamples):
     allSamples = []
@@ -25,3 +26,8 @@ def artSamples(oriSamples):
         allSamples.append(np.rot90(np.rot90(imgArr)))
 
     return allSamples
+
+#%%
+copySamples('./trainingSet', 60)
+
+#%%
