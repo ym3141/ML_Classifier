9f0e29948ed379fd44aeefac2ba29eb193a798be
ym3141 <yma2@caltech.edu>
2019-10-31 22:39:07 -0700

    updated how training set is used / generated

--- genTrainSet.py
+++ genTrainSet.py
@@ -4,6 +4,8 @@ from PIL import Image
 from os import listdir
 from shutil import copyfile
 
+
+# Legancy codes
 imageDir1 = 'D:/Snapshots/2019-08-23_Scanning/Scan2019-08-23 22.23.41'
 imageDir2 = 'D:/Snapshots/2019-08-23_Scanning/Scan2019-08-25 22.27.41'
 
@@ -15,7 +17,9 @@ def copySamples(targetDir, shift):
 
     for idx, file in enumerate(randList):
         copyfile(file, '{0}/trainSample_{1:03d}.tiff'.format(targetDir, idx + shift))
-    
+
+
+# Generate artifical samples by fliping (2) and rotating (1).
 def artSamples(oriSamples):
     allSamples = []
     # oriSamples = []
@@ -27,10 +31,29 @@ def artSamples(oriSamples):
 
     return allSamples
 
+
+# Load ground truth from a certain dir, returns a array of TrueOrFalse, and a array of images, unshuffled
+def loadGT(tsDir):
+    trueSamples = []
+    for imgName in listdir('{0}/True/'.format(tsDir)):
+        if imgName.endswith('tif'):
+            imgArr = np.array(Image.open('{0}/True/{1}'.format(tsDir, imgName)))
+            trueSamples.append(imgArr)
+    falseSamples = []
+    for imgName in listdir('{0}/False/'.format(tsDir)):
+        if imgName.endswith('tif'):
+            imgArr = np.array(Image.open('{0}/False/{1}'.format(tsDir, imgName)))
+            falseSamples.append(imgArr)
+
+    truthArr = [True] * len(trueSamples) + [False] * len(falseSamples)
+    smpArr = trueSamples + falseSamples
+
+    return truthArr, smpArr
+
+
 #%%
 if __name__ == '__main__':
-    copySamples('./trainingSet', 100)
-
+    ss = loadGT('./TrainingSets/TS2')
 
 
 #%%
